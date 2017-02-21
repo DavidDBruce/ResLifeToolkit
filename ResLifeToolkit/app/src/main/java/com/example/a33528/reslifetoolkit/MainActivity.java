664aa63f24530393289b26f73ec9e8b689298866
s522706 <s522706@mail.nwmissouri.edu>
2017-02-21 10:13:38 -0600

    Come classes written, some pages linked.

--- ResLifeToolkit/app/src/main/java/com/example/a33528/reslifetoolkit/MainActivity.java
+++ ResLifeToolkit/app/src/main/java/com/example/a33528/reslifetoolkit/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.a33528.reslifetoolkit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_screen);
+    }
+    public void programmingForms(View view)
+    {
+        Intent intent = new Intent(this,ProgrammingListActivity.class);
+        startActivity(intent);
+    }
+    public void dutyLogs(View view)
+    {
+        Intent intent = new Intent(this,DutyLogList.class);
+        startActivity(intent);
     }
 }
