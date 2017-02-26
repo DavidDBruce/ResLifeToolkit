f1122c21e6e09491993484c21e1711c5f6cad4c3
s522706 <s522706@mail.nwmissouri.edu>
2017-02-25 19:20:16 -0600

    ArrayAdapters furthered

--- ResLifeToolkit/app/src/main/java/com/example/a33528/reslifetoolkit/ProgrammingListActivity.java
+++ ResLifeToolkit/app/src/main/java/com/example/a33528/reslifetoolkit/ProgrammingListActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -16,11 +18,43 @@ public class ProgrammingListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generic_list);
+
         ProgrammingForm testForm = new ProgrammingForm("David","South",2,"Smash Night","Fun","Playing Smash Bros", "Flyers and Emails", 0.0, 12, "yes");
         ArrayList<ProgrammingForm> formsList = new ArrayList<ProgrammingForm>();
         formsList.add(testForm);
-        ListAdapter programmingList = new ArrayAdapter<ProgrammingForm>(this, R.layout.programming_list_item, formsList);
 
+        String[] names = {"David","Cheyanne","Sadie","Priyanka","Addi"};
+
+        ListAdapter programmingListLA = new ArrayAdapter<String>(this,R.layout.generic_list,R.id.formTypeTV,formsList);
+        //ListAdapter programmingListLA = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+        ListView programmingLV = (ListView) findViewById(R.id.genericLV);
+        programmingLV.setAdapter(programmingListLA);
+
+
+    }
+}
+
+class ProgrammingAdapter extends BaseAdapter
+{
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
     }
 }
 
